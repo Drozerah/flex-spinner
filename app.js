d514ffd9a32fd3d06d6f6849cd14e530298f9a91
Drozerah <drozerah@gmail.com>
2020-02-20 17:37:23 +0100

    feat: add last changes

--- app.js
+++ app.js
@@ -1,6 +1,48 @@
 (() => {
 
-    console.log('JS is loaded!')
-    // put your code below
+    class Spinner {
+        constructor(spinner){
+            this._spinner = spinner
+            this._isRotate = false
+        }
+
+        talk(){
+            console.log(this._spinner) // !DEBUG
+        }
+        /**
+        * Add Class Rotate 
+        */
+        addClassRotate(){
+            this._spinner.classList.add('rotate')
+
+        }
+        /**
+        * Remove Class Rotate 
+        */
+        removeClassRotate(){
+            this._spinner.classList.remove('rotate')
+        }
+        /**
+        * Toogle Class Rotate 
+        */
+        toogleClassRotate(){
+            if (!this._isRotate) {
+                this.addClassRotate() 
+            } else {
+                this.removeClassRotate()
+            }
+            this._isRotate = !this._isRotate
+        }
+    }
+
+    // Instanciate New Spinner Element
+    const spinner = new Spinner(document.querySelector('#spinner'))
+    const button = document.querySelector('button')
+        
+    button.addEventListener('click',(e) => {
+        // Tooge Rotate
+        spinner.toogleClassRotate()
+    })
+
     
 })()
